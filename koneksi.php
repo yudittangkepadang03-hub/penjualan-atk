a499bb626fef4dda2c886b02ccaf4ff92dc9b84c
yudittangkepadang03-hub <emailkamu@gmail.com>
2026-06-29 19:58:31 +0800

    update koneksi Railway

--- koneksi.php
+++ koneksi.php
@@ -1,11 +1,11 @@
 <?php
-$host = "localhost";
+$host = "mysql.railway.internal";
 $user = "root";
-$pass = "";
-$db   = "penjualan_atk";
+$pass = "dDQNKaSWhvYmayFgjRRMaaNzhFdETCqA";
+$db   = "railway";
+$port = 3306;
 
-$conn = mysqli_connect($host, $user, $pass, $db);
-
+$conn = mysqli_connect($host, $user, $pass, $db, $port);
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
